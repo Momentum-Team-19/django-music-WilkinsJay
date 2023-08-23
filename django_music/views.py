f09992f9b3f64d57d1262173e766dd4c446f063a
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-08-23 16:11:07 -0400

    album, song, artist display

---
+++ django_music/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Album
+
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, 'album_list.html', {'albums': albums})
+
+
+def album_detail(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, 'album_detail.html', {'album': album})
